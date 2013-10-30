cd8c23965890411aa5f14b920efd221165cec6fa
Fermat <fermat1217@gmail.com>
2013-10-30 01:04:03 -0500

    change the mainpage.

--- script.sh
+++ script.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 echo change mode
 chmod og+r index.html
+chmod og+r ./src/publications/publications.html
+chmod og+r ./src/notes/notes.html
+chmod og+r ./src/talks/talks.html
+chmod og+r ./src/cv/cv.html
+
+chmod og+r ./src/cv/cv.pdf
+chmod og+r ./document/papers/*
+chmod og+r ./document/talks/*
+chmod og+r ./document/notes/*
+
