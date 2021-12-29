6cc9b27ae7796ba10b057657717feeb51251515c
Peng Fu <fu@chase.mathstat.dal.ca>
2021-12-29 11:44:42 -0400

    make a redirection to my actual homepage

--- myjs.js
+++
@@ -1,8 +0,0 @@
-// What's happening is that appendChild
-// removes the node from the document before appending it. 
-function reverse(its){
-    var ns = document.getElementById(its);
-    var i = ns.childNodes.length;
-    while (i--)
-        ns.appendChild(ns.childNodes[i]);
-}
